a48b7ea0f2b6ec9a4ae2977853cae4ebbf134a6b
Enrico Lucia <enrico.lucia.web@gmail.com>
2018-04-18 20:36:26 +0200

    Styling own and not own message

--- src/client/components/Message.js
+++ src/client/components/Message.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { darken } from 'polished'
 import styled from 'styled-components'
 
+const yourBubblePrimary = '#329cd4'
 const myBubblePrimary = '#AFFFC9'
 
 const StyledMessage = styled.div`
@@ -9,31 +10,49 @@ const StyledMessage = styled.div`
   border-radius: 4px;
   margin: 10px;
   position: relative;
-  border: 1px solid ${darken(0.1, myBubblePrimary)};
-  border-color: ${myBubblePrimary};
-  background: ${myBubblePrimary};
   border-style: solid;
   border-width: 1px 0 1px;
   max-width: 70%;
-  margin-left: auto;
-  margin-right: 10px;
 
   :before {
     content: '';
     position: absolute;
     display: block;
-    border-width: 6px 0 6px 10px;
-    border-color: transparent ${myBubblePrimary};
-    right: -11px;
-    bottom: 2px;
     z-index: 1;
     border-style: solid;
   }
+
+  ${props => props.data.own && `
+    background: ${myBubblePrimary}
+    margin-left: auto;
+    margin-right: 10px;
+    border: 1px solid ${darken(0.1, myBubblePrimary)};
+
+    :before {
+      border-width: 6px 0 6px 10px;
+      border-color: transparent ${darken(0.1, myBubblePrimary)};
+      right: -11px;
+      bottom: 2px;
+    }
+  `}
+  ${props => !props.data.own && `
+    background: ${yourBubblePrimary}
+    margin-right: auto;
+    margin-left: 10px;
+    border: 1px solid ${darken(0.1, yourBubblePrimary)};
+
+    :before {
+      border-width: 6px 10px 6px 0;
+      border-color: transparent ${darken(0.1, yourBubblePrimary)};
+      left: -11px;
+      bottom: 2px;
+    }
+  `}
 }`
 
-const Message = ({ data }) => {
+const Message = ({ data, isCountdown }) => {
   return (
-    <StyledMessage>
+    <StyledMessage data={data}>
       {data.content}
     </StyledMessage>
   )
