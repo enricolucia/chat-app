2f32f7b0897ffcc4072f49a24f32ce2ab87dbef4
Enrico Lucia <enrico.lucia.web@gmail.com>
2018-04-18 17:57:29 +0200

    Implement basic socket on server

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const webpack = require('webpack')
+const path = require('path')
+const uuid = require('uuid/v4')
 
 const app = express()
 const port = 3000
+const connections = []
 
-console.log('Server is running on development mode')
+if (process.env.NODE_ENV === 'development') {
+  console.log('Server is running on development mode')
 
-const config = require('./webpack.dev.config')
-const compiler = webpack(config)
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  logLevel: 'warn'
-}))
-app.use(require('webpack-hot-middleware')(compiler, {
-  log: console.log
-}))
-app.use(express.static(__dirname))
+  const config = require('./webpack.dev.config')
+  const compiler = webpack(config)
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    logLevel: 'warn'
+  }))
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log
+  }))
+  app.use(express.static(__dirname))
+} else {
+  app.use(express.static(path.join(__dirname, process.env.DIST_PATH)))
+}
 
 const server = app.listen(port, function () {
   console.log(`listening on port ${port}`)
@@ -25,4 +32,10 @@ const server = app.listen(port, function () {
 // Initialize socket
 var io = require('socket.io')(server)
 
-io.on('connection', function (socket) {})
+io.on('connection', function (socket) {
+  // User connected
+  connections.push(socket)
+  const userId = uuid()
+
+  socket.emit('user:connect', { userId })
+})
