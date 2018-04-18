d6bd0c5c7dfcb8c0c25f756671d75cecc0de92c5
Enrico Lucia <enrico.lucia.web@gmail.com>
2018-04-18 17:54:43 +0200

    Adding production web pack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+
+module.exports = merge(common, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new MinifyPlugin()
+  ]
+})
