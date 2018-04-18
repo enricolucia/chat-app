8b1296e5ef797e3db99bffed5c2bac86d95d1e8f
Enrico Lucia <enrico.lucia.web@gmail.com>
2018-04-18 19:41:39 +0200

    Define typing constant command and action

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,6 +1,11 @@
 import {
   SET_ID,
-  NEW_MESSAGE
+  NEW_MESSAGE,
+  SET_TYPING,
+  RESET_TYPING,
+
+  CMD_TYPING,
+  CMD_NOT_TYPING
 } from '../constants'
 
 export const setId = (payload) => ({ type: SET_ID, payload })
@@ -15,6 +20,33 @@ export const newMessage = ({
   }
 })
 
+export const setTyping = (payload) => ({
+  type: SET_TYPING,
+  emit: true,
+  payload
+})
+
+export const resetTyping = (payload) => ({
+  type: RESET_TYPING,
+  payload,
+  emit: true
+})
+
+const mappedActions = {
+  [CMD_TYPING]: setTyping,
+  [CMD_NOT_TYPING]: resetTyping
+}
+
 export const dispatchText = ({ content, id }) => {
+  if (content.startsWith('/')) {
+    const splitted = content.split(' ')
+
+    const mappedAction = mappedActions[splitted[0]] || newMessage
+
+    return mappedAction({
+      id,
+      content: splitted.slice(1).join(' ')
+    })
+  }
   return newMessage({id, content})
 }
--- src/client/constants/index.js
+++ src/client/constants/index.js
@@ -1,2 +1,4 @@
 export const NEW_MESSAGE = 'NEW_MESSAGE'
 export const SET_ID = 'SET_ID'
+export const SET_TYPING = 'SET_TYPING'
+export const RESET_TYPING = 'RESET_TYPING'
